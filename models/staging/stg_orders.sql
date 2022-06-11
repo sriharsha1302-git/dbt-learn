d0faa368a2b42718063bcffa95f2a0c1fc12907f
learnasyougo.shah@gmail.com <learnasyougo.shah@gmail.com>
2022-06-11 17:20:33 +0000

    developed stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,25 @@
+SELECT 
+-- from raw_orders
+Ordr.orderid,
+Ordr.orderdate,
+Ordr.shipdate,
+Ordr.shipmode,
+Ordr.ordercostprice,
+Ordr.ordersellingprice,
+(Ordr.ordersellingprice - Ordr.ordercostprice) as orderprofit,
+--from raw_customer
+Cust.customername,
+Cust.segment,
+Cust.country,
+--from raw_product
+Prod.category,
+Prod.productname,
+Prod.subcategory
+FROM
+{{ ref('raw_orders') }} as Ordr
+LEFT JOIN
+{{ ref('raw_customer') }} as Cust
+on Ordr.customerid = Cust.customerid
+LEFT JOIN
+{{ ref('raw_product') }} as Prod
+on Ordr.productid = Prod.productid
